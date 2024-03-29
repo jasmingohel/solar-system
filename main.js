fd11c0d17ad29f9fae7eaba564d6e4ba24b6f48a
Jasmin Gohel <jasmin.gohel@krishtechnolabs.com>
2024-03-29 18:59:04 +0530

    added movement

--- main.js
+++ main.js
@@ -293,32 +293,32 @@ function animate() {
 
   // mercury's orbit around the sun
   mercuryShape.position.x = 10 * Math.cos(step);
-  mercuryShape.position.y = 10 * Math.sin(step);
+  mercuryShape.position.y = 1 - 20 * Math.sin(step);
   mercuryShape.position.z = 10 * Math.sin(step);
 
   // venus's orbit around the sun
   venusShape.position.x = 12 * Math.cos(step);
-  venusShape.position.y = 12 * Math.sin(step);
+  venusShape.position.y = 2 + 24 * Math.sin(step);
   venusShape.position.z = 12 * Math.sin(step);
 
   // mars's orbit around the sun
   marsShape.position.x = 14 * Math.cos(step);
-  marsShape.position.y = 14 * Math.sin(step);
+  marsShape.position.y = 3 - 28 * Math.sin(step);
   marsShape.position.z = 14 * Math.sin(step);
 
   // Earth's orbit around the sun
   sphere.position.x = 17 * Math.cos(step);
-  sphere.position.y = 17 * Math.sin(step);
+  sphere.position.y = 2 + 34 * Math.sin(step);
   sphere.position.z = 17 * Math.sin(step);
 
   // jupiter's orbit around the sun
   jupiterShape.position.x = 25 * Math.cos(step);
-  jupiterShape.position.y = 25 * Math.sin(step);
+  jupiterShape.position.y = 7 + 50 * Math.sin(step);
   jupiterShape.position.z = 25 * Math.sin(step);
 
   // saturn's orbit around the sun
   saturnShape.position.x = 35 * Math.cos(step);
-  saturnShape.position.y = 35 * Math.sin(step);
+  saturnShape.position.y = 70 * Math.sin(step) - 10;
   saturnShape.position.z = 35 * Math.sin(step);
 
   // satrun ring
@@ -331,12 +331,12 @@ function animate() {
 
   // uranus's orbit around the sun
   uranusShape.position.x = 42 * Math.cos(step);
-  uranusShape.position.y = 42 * Math.sin(step);
+  uranusShape.position.y = 5 + 84 * Math.sin(step);
   uranusShape.position.z = 42 * Math.sin(step);
 
   // neptune's orbit around the sun
   neptuneShape.position.x = 49 * Math.cos(step);
-  neptuneShape.position.y = 49 * Math.sin(step);
+  neptuneShape.position.y = 3 - 98 * Math.sin(step);
   neptuneShape.position.z = 49 * Math.sin(step);
 
   // Moon's orbit around the earth
@@ -347,7 +347,13 @@ function animate() {
     sphere.position.y + moonOrbitRadius * Math.sin(moonStep);
   moonShape.position.z =
     sphere.position.z + moonOrbitRadius * Math.cos(moonStep);
-  console.log(mercuryShape.position.x, sphere.position.x, moonShape.position.x);
+  console.log(
+    mercuryShape.position,
+    sphere.position,
+    moonShape.position,
+    uranusShape.position,
+    saturnShape.position
+  );
   // Rotate the moon around the earth
   // moonShape.rotation.y = 2 * options.speed; // Adjust the speed as needed
 
